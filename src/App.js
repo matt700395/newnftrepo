e866f6ea34df5111cdc781858eb0f774507eb137
matt700395 <matt7003@korea.ac.kr>
2022-06-13 17:40:09 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Web3 from "web3";
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [web3, setWeb3] = useState();
+
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {// window.ethereum이 있다면
+      try {
+        const web = new Web3(window.ethereum); // 새로운 web3 객체를 만든다
+        setWeb3(web);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, []);
+
+  const connectWallet = async () => {
+    // 메타마스크 지갑과 연결된 계정 정보를 받는 JSON-RPC Call API
+    let accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    setAccount(accounts[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="metaConnect"
+        onClick={() => {
+          connectWallet();
+        }}>
+        connect to MetaMask
+      </button>
+      {/* // 연결된 계정 주소를 화면에 출력합니다 */}
+      <div className="userInfo">주소: {account}</div>
     </div>
   );
 }
